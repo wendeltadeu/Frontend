354517ce32cc78c6e74805ada9ab068ac9b741be
Wendel Tadeu <wendel_tadeu@yahoo.com.br>
2022-01-28 11:32:51 -0300

    Exercicios React

--- reactprojects/my-app/src/App.js
+++ reactprojects/my-app/src/App.js
@@ -2,6 +2,27 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (<h1>Olá mundo!</h1>);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          Meu primeiro projeto do zero em react
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
+
 export default App;
