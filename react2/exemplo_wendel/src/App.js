73bad56248c49e6d6f7d74bee5679234f3c54a29
Wendel Tadeu <wendel_tadeu@yahoo.com.br>
2022-01-28 18:40:55 -0300

    segundo exercicio de react

---
+++ react2/exemplo_wendel/src/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import './App.css';
+import Person from './person';
+
+return (  
+  <div>
+    <center>
+    <h1>Componente pessoa: </h1>
+    <Person />
+    </center>
+  </div>
+    );
+
+
+export default App;
---
+++ react2/exemplo_wendel/src/Person.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Person extends Component{
+    render(){
+        return(
+            <div>Oi</div>
+        );
+    }
+}
+
+export default Person;
