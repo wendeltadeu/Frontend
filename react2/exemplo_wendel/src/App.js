d58c2f093a2ae2bc74aa9988ea45961ff38f4766
Wendel Tadeu <wendel_tadeu@yahoo.com.br>
2022-01-28 19:53:21 -0300

    exercicio de props e states em react2

--- react2/exemplo_wendel/src/App.js
+++ react2/exemplo_wendel/src/App.js
@@ -1,15 +1,15 @@
 import React, {Component} from 'react';
 import './App.css';
-import Person from './person';
+import Person from './Person';
 
-return (  
-  <div>
-    <center>
-    <h1>Componente pessoa: </h1>
-    <Person />
-    </center>
-  </div>
-    );
-
+function App() {
+    return(
+      <div>
+        <center>
+        <h1>Componente pessoa: </h1>
+        <Person name="Wendel Tadeu" age={49} genre='m' />
+      </div>
+   )
+}
 
 export default App;
--- react2/exemplo_wendel/src/Person.js
+++ react2/exemplo_wendel/src/Person.js
@@ -1,9 +1,33 @@
 import React, {Component} from 'react';
 
-class Person extends Component{
-    render(){
+class Person extends Component{ 
+    constructor(props){
+        super(props);
+
+        this.state = {
+            name: "Wendel",
+            age: 33,
+            genre: 'm',
+        };
+    }
+
+    changePerson(){
+        alert('teste!');
+    }
+
+    render(props){
+        let artigo;
+        if (this.state.genre =='m'){
+            artigo = 'o';
+        }else if(this.state.genre =='f'){
+            artigo = 'a';
+        }
+
         return(
-            <div>Oi</div>
+            <div>
+                <p>Eu sou {artigo} {this.state.name} e tenho {this.state.age} anos </p> n
+                <button type='button' onAuxClick={this.changePerson}>Mudar Pessoa</button>
+            </div>
         );
     }
 }
