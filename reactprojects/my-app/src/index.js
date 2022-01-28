655418d8952ea4ca049c81d79dc40ce7f417b522
Wendel Tadeu <wendel_tadeu@yahoo.com.br>
2022-01-28 09:35:06 -0300

    exercicios iniciais com React

--- reactprojects/my-app/src/index.js
+++ reactprojects/my-app/src/index.js
@@ -4,12 +4,36 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const jsx = (
+  <ul>
+      <li>Universo Discreto</li>
+      <li>Peixe Babel</li>
+      <li>Science Vlogs Brasil</li>
+  </ul>
 );
+
+ReactDOM.render(jsx, document.getElementById('root'));
+
+
+//const jsx = <div>O fatorial de 3 é {3 * 2 * 1}</div>
+//ReactDOM.render(jsx, document.getElementById('root'));
+
+//const withJSX = <h1>Eu sou Javascript</h1>;
+//const withoutJSX = React.createElement('h1', {}, 'Eu sou Javascript!');
+
+//você poderia mudar o primeiro parâmetro para withoutJSX que funcionaria igual. Os dois representam a mesma coisa, mas escrever no formato JSX é muito mais fácil
+//ReactDOM.render(withJSX, document.getElementById('root'));
+
+
+//const jsx = <div>Outro Teste!!</div>
+//ReactDOM.render(jsx, document.getElementById('root'));
+
+//ReactDOM.render(
+//  <React.StrictMode>
+//    <App />
+//  </React.StrictMode>,
+//  document.getElementById('root')
+//);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
