1744f4f50b77978eaaea8c68494b7b7cf044d7c3
Wendel Tadeu <wendel_tadeu@yahoo.com.br>
2022-01-30 20:44:33 -0300

    Exercicios Components, Props e State

--- react2/exemplo_wendel/src/Person.js
+++ react2/exemplo_wendel/src/Person.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-
+ 
 class Person extends Component{ 
     constructor(props){
         super(props);
---
+++ tarefa2/login/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     
+      <input type="text" name="input" value="Digite aqui"></input>
+     
+    </div>     
+  );
+}
+
+
+
+export default App;
