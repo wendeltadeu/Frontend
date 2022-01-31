c2575f1a29cdc390ae11acab18708191ae0bd047
Wendel Tadeu <wendel_tadeu@yahoo.com.br>
2022-01-30 23:06:43 -0300

    Correção de campo nome usuario - componente login

--- tarefa2/login/src/UsuarioSaida.js
+++ tarefa2/login/src/UsuarioSaida.js
@@ -1,7 +1,7 @@
 export function UsuarioSaida() {
     return (
       <div>
-        <p>Nome: Wendel</p>
+        <p>Nome:</p>
         <p>Tarefa de Frontend componente Login</p>
        
       </div>     
