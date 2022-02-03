69cc92d9c76470150f5149dde4399ad0302be41f
Wendel Tadeu <wendel_tadeu@yahoo.com.br>
2022-02-03 12:25:55 -0300

    correções componente login

--- tarefa2/login/src/UsuarioEntrada.js
+++ tarefa2/login/src/UsuarioEntrada.js
@@ -1,7 +1,7 @@
 export function UsuarioEntrada(){
     return (
       <div>
-        <input type="text" name="input" value="Digite aqui" formMethod="post"></input>
+        <input type="text" name="input" ></input>
       </div>     
     );
   }
